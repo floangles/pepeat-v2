d448571977278edeabd0963eb70051bdac0cd9ec
florentangles <florent.angles@hei.fr>
2015-10-05 10:14:42 +0200

    change config.rb for asset precomplie false

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module PepeatV2
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :fr
     config.embed_authenticity_token_in_remote_forms = true
