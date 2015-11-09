fb2993d9f2bac7c0c6a0bb4fdc3228ce470016e5
florentangles <florent.angles@hei.fr>
2015-11-09 14:32:50 +0100

    change connect key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@ if(Rails.env == 'development' || Rails.env == 'staging')
   Rails.configuration.stripe = {
     publishable_key: 'pk_test_AnTNRrYA5hnK0DRMzVqIOMUw',
     secret_key:      'sk_test_E09VifanTXsnC6sNtJRxKpjk',
-    connect_client_id:  'ca_7ATbm44iBE4AsxcmbWjU3Bk4FiXZbOzB'
+    client_id:  'ca_7ATbm44iBE4AsxcmbWjU3Bk4FiXZbOzB'
   }
 
   Stripe.api_key = 'sk_test_E09VifanTXsnC6sNtJRxKpjk'
@@ -12,7 +12,7 @@ elsif(Rails.env == 'production')
   Rails.configuration.stripe = {
     publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
     secret_key:      ENV['STRIPE_SECRET_KEY']
-    connect_client_id:  ENV['STRIPE_CONNECT_CIENT_ID']
+    client_id:  ENV['STRIPE_CONNECT_CIENT_ID']
   }
 
   Stripe.api_key = ENV['STRIPE_SECRET_KEY']
