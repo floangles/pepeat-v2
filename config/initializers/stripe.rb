d6b288403cab2f261b711e74b79a92e4fd69b59b
florentangles <florent.angles@hei.fr>
2015-11-11 22:03:38 +0100

    stripe init

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,20 +1,10 @@
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key:      ENV['STRIPE_SECRET_KEY'],
+  client_id:  ENV['STRIPE_CONNECT_CIENT_ID']
+}
 
-if(Rails.env == 'development' || Rails.env == 'staging')
-  Rails.configuration.stripe = {
-    publishable_key: 'pk_test_AnTNRrYA5hnK0DRMzVqIOMUw',
-    secret_key:      'sk_test_E09VifanTXsnC6sNtJRxKpjk',
-    client_id:  'ca_7ATbm44iBE4AsxcmbWjU3Bk4FiXZbOzB'
-  }
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-  Stripe.api_key = 'sk_test_E09VifanTXsnC6sNtJRxKpjk'
 
-elsif(Rails.env == 'production')
-  Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key:      ENV['STRIPE_SECRET_KEY'],
-    client_id:  ENV['STRIPE_CONNECT_CIENT_ID']
-  }
 
-  Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-end
