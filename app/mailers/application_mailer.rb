67704d45d5fb1809ecfdf1a56a06d892ddc34996
florentangles <florent.angles@hei.fr>
2015-11-19 13:50:22 +0100

    send mail heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "team@pepeat.org"
-  layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'pepeat-v2-production.herokuapp.com' }
+  config.action_mailer.asset_host = 'https://pepeat-v2-production.herokuapp.com'
   config.action_mailer.perform_deliveries = true
 
 
