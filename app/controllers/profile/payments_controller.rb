1db02e4180ae407f0d6cb8cd6f203b387b9fa3cc
florentangles <florent.angles@hei.fr>
2015-11-24 10:56:09 +0100

    bg job mailer test

--- app/controllers/profile/payments_controller.rb
+++ app/controllers/profile/payments_controller.rb
@@ -29,7 +29,7 @@ module Profile
     end
 
     PaymentMailer.confirmation(@order).deliver_now
-    PaymentMailer.delay_until(5.minutes.from_now).remember(@order.id)
+    PaymentMailer.delay_for(5.minutes).remember(@order)
     # You should store this customer id and re-use it.
     @commission = @order.amount_cents * @order.portion.to_i * @client + @order.amount_cents * @order.portion.to_i * @chief
     charge = Stripe::Charge.create({
