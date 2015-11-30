2ddce0f45e809657d8d11fc14e30d76af2280f83
florentangles <florent.angles@hei.fr>
2015-11-30 23:27:36 +0100

    change countdown date

--- app/assets/javascripts/app/countdown.js
+++ app/assets/javascripts/app/countdown.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   $('#countdown').countdown({
-    until: new Date(2016, 01, 15), labels: ['Yr', 'Mo', 'Wk', 'Jours', 'Heures', 'Minutes', 'Secondes'], labels1: ['Yr1', 'Mo1', 'Wk1', 'Jour', 'Heure', 'Minute', 'Seconde'], });
+    until: new Date(2016, 1 - 1, 15), labels: ['Yr', 'Mo', 'Wk', 'Jours', 'Heures', 'Minutes', 'Secondes'], labels1: ['Yr1', 'Mo1', 'Wk1', 'Jour', 'Heure', 'Minute', 'Seconde'], });
 });
