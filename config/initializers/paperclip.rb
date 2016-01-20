ac815c5e2e6609934596be7db69c794f306bc49f
florentangles <florent.angles@hei.fr>
2016-01-20 18:32:15 +0100

    s3 protocol https

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:protocol] = :https
+Paperclip::Attachment.default_options[:s3_protocol] = :https
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:rails_env/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_credentials] = {
