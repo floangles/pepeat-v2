3c1056b7b87779fd4332d6f2bfb445b0e7637db2
florentangles <florent.angles@hei.fr>
2016-02-09 15:35:40 +0100

    finish desgin events

--- app/assets/javascripts/app/scroll_reveal.js
+++ app/assets/javascripts/app/scroll_reveal.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 window.sr = ScrollReveal();
-sr.reveal('.foo',{ duration: 800, delay: 700, mobile: true });
+sr.reveal('.foo',{ duration: 1100, delay: 100, mobile: true });
+sr.reveal('.bar',{ duration: 3000, mobile: true, origin: 'bottom', distance: '200px', });
 })
 
 
