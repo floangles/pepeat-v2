d70924c0052ebd2d14a168d01506bb5796e338da
florentangles <florent.angles@hei.fr>
2016-02-18 18:21:21 +0100

    change dispo meal show

--- app/assets/javascripts/app/scroll_reveal.js
+++ app/assets/javascripts/app/scroll_reveal.js
@@ -3,6 +3,7 @@ window.sr = ScrollReveal();
 sr.reveal('.foo',{ duration: 1100, delay: 100, mobile: true });
 sr.reveal('.bar',{ duration: 2000, mobile: true, origin: 'bottom', distance: '100px', });
 sr.reveal('.up',{ duration: 2000, mobile: true, origin: 'bottom', distance: '50px', });
+sr.reveal('.sup',{ duration: 500, mobile: true, origin: 'bottom', distance: '30px', });
 })
 
 
