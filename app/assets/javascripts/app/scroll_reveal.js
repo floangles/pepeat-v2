f7ff141872e74e6c21950d9e915a0f2a5424f3dc
florentangles <florent.angles@hei.fr>
2016-02-18 11:43:46 +0100

    user home

--- app/assets/javascripts/app/scroll_reveal.js
+++ app/assets/javascripts/app/scroll_reveal.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 window.sr = ScrollReveal();
 sr.reveal('.foo',{ duration: 1100, delay: 100, mobile: true });
 sr.reveal('.bar',{ duration: 2000, mobile: true, origin: 'bottom', distance: '100px', });
+sr.reveal('.up',{ duration: 2000, mobile: true, origin: 'bottom', distance: '50px', });
 })
 
 
